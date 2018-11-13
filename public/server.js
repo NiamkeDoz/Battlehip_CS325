2d522d60e5b78249d0b328b3fb854fe4c037c135
Unknown <ehaysle@siue.edu>
2018-11-13 13:33:15 -0600

    "changes made"

--- public/server.js
+++ public/server.js
@@ -1,17 +1,45 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+player = {
+    'playerName': null,
+    'airCraftPosition': null,
+    'battleShipPosition': null,
+    'destroyerPosition': null,
+    'submarinePosition': null,
+    'patrolboatPosition': null
+};
+
+opponent = {
+    'opponentName': null,
+    'airCraftPosition': null,
+    'battleShipPosition': null,
+    'destroyerPosition': null,
+    'submarinePosition': null,
+    'patrolboatPosition': null
+};
 
 app.get('/', (req,res) => {
     res.sendFile('index.html')
 });
 
+// Position params will be taken as an x and y and the ship
+app.post('/createGame', (req,res) =>{
+    // var playerName = req.query.playerName;
+    var playerBoard = req.query.playerBoard;
+    res.send(playerBoard);
+    // var data = req.params;
+    // var playerName = data.playerName;
+    // var ship = data.battleShip;
+    // var shipPositionX = data.shipPositionX;
+    // var shipPositionY = data.shipPositionY;
+});
 
-// var path = require('path');
-// var app = express();
 
-// app.use(express.static('public'))
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Listening port 3000"));
